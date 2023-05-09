3faaa0f07dec312a22f36549c02cd1e096c4aed9
Grzegorz Smoktunowicz <biarth@wp.pl>
2023-05-09 21:16:29 +0200

    graphql section

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -21,6 +21,7 @@ type application struct {
 	JWTIssuer    string
 	JWTAudience  string
 	CookieDomain string
+	APIKey       string
 }
 
 func main() {
@@ -32,6 +33,7 @@ func main() {
 	flag.StringVar(&app.JWTAudience, "jwt-audience", "example.com", "signing audience")
 	flag.StringVar(&app.CookieDomain, "cookie-domain", "localhost", "cookie domain")
 	flag.StringVar(&app.Domain, "domain", "example.com", "domain")
+	flag.StringVar(&app.APIKey, "api-key", "", "api key")
 	flag.Parse()
 
 	conn, err := app.connectToDB()
